15c13838821d0abadc44398a5b1d755f9a619813
Zeyu Yan <zyan01@utbm.fr>
2019-09-28 15:47:24 +0200

    YAN ZEYU

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,13 @@
-require 'test_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
 
-class StudentTest < ActiveSupport::TestCase
-  test "should have the necessary required validators" do
-    student = Student.new
-    assert_not student.valid?
-    assert_equal [:name, :last_name], student.errors.keys
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
 
-    student.name = "Gerard"
-    student.last_name= "Bouchard"
-    assert student.valid?
-  end
+  # Add more helper methods to be used by all tests here...
 end
+
+
 
