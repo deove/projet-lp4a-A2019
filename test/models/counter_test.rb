9f8ef7a9ddb4b771d931b7a0dedd64bf36a8c760
deove <52627348+deove@users.noreply.github.com>
2019-10-06 06:08:02 +0800

    Update counter_test.rb

--- test/models/counter_test.rb
+++ test/models/counter_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
+
+
 class CounterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should increment counter valeur" do
+
+    counter = Counter.new
+
+    counter.valeur = 0
+
+
+
+    counter.increment!
+
+
+
+    assert counter.valeur == 1
+
+  end
+
 end
